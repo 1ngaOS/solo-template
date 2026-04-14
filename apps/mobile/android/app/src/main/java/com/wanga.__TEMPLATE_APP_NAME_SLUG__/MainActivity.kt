983debbae113573c8f36dfc4bc943a64060adc14
Mitch Chanza <mitch.chanza@gmail.com>
2026-04-14 23:35:39 +0200

    Add mobile app template with placeholder support

---
+++ apps/mobile/android/app/src/main/java/com/wanga.__TEMPLATE_APP_NAME_SLUG__/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.wanga.__TEMPLATE_APP_NAME_SLUG__
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "__TEMPLATE_APP_NAME_SLUG__"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
